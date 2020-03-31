0b5db2d0ca2c6baf62db023a453527bc5c2bdd53
smcaloni <smack000@gmail.com>
2020-03-31 17:04:11 -0400

    18-handling-auth-api-actions-challenge

--- src/app/shared/state/auth.reducer.ts
+++ src/app/shared/state/auth.reducer.ts
@@ -29,8 +29,30 @@ export interface State {
         user: null,
         error: null
       };
+    }),
+    on(AuthApiActions.getAuthStatusSuccess, (state, action) => {
+      return {
+        gettingStatus: false,
+        user: action.user,
+        error: null
+      };
+    }),
+    on(AuthApiActions.loginSuccess, (state, action) => {
+      return {
+        gettingStatus: false,
+        user: action.user,
+        error: null
+      };
+    }),
+    on(AuthApiActions.loginFailure, (state, action) => {
+      return {
+        gettingStatus: false,
+        user: null,
+        error: action.reason
+      };
     })
   );
+  
   
   // Makes it AOT compatible
   export function reducer(state: State | undefined, action: Action) {
